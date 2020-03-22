944ce5d5b058fccb8302c549f19cc192d44f4346
Mauricio Arias <42188044+Kuraimauri@users.noreply.github.com>
2020-03-21 20:13:57 -0500

    Construcción de Software 5

---
+++ 04-ServicioWeb/04-ConsumoServicioWeb/MainActivity.cs
@@ -0,0 +1,166 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace _04_ConsumoServicioWeb
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btnConsultar = FindViewById<Button>(2131230759);
+            Button btnInsertar = FindViewById<Button>(2131230760);
+
+            EditText txtID = FindViewById<EditText>(2131230908);
+            EditText txtTitulo = FindViewById<EditText>(2131230909);
+            EditText txtContenido = FindViewById<EditText>(2131230907);
+
+            string uriServicio = "https://jsonplaceholder.typicode.com/posts";
+
+            btnConsultar.Click += async (sender, e) =>
+            {
+                try
+                {
+                    Cliente cliente = new Cliente();
+                    if (!string.IsNullOrWhiteSpace(txtID.Text))
+                    {
+                        int id = 0;
+                        if (int.TryParse(txtID.Text.Trim(), out id))
+                        {
+                            var resultado = await cliente.Get<Entrada>(uriServicio + "/" + txtID.Text);
+                            if (cliente.codigoHTTP == 200)
+                            {
+                                txtTitulo.Text = resultado.title;
+                                txtContenido.Text = resultado.body;
+                                Toast.MakeText(this, "Consulta realizada con éxito", ToastLength.Long).Show();
+                            }
+                            else
+                            {
+                                throw new Exception(cliente.codigoHTTP.ToString());
+                            }
+                        }
+                        else
+                        {
+                            Toast.MakeText(this, "Por favor ingrese un número entero como ID", ToastLength.Long).Show();
+                        }
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Por favor ingrese un ID", ToastLength.Long).Show();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, "Error: " + ex.Message, ToastLength.Long).Show();
+                }
+            };
+
+            btnInsertar.Click += async (sender, e) =>
+            {
+                try
+                {
+                    Cliente cliente = new Cliente();
+                    Entrada entrada = new Entrada();
+
+                    entrada.title = txtTitulo.Text;
+                    entrada.body = txtContenido.Text;
+
+                    if (!string.IsNullOrWhiteSpace(txtTitulo.Text) && !string.IsNullOrWhiteSpace(txtContenido.Text))
+                    {
+                        var resultado = await cliente.Post<Entrada>(entrada, uriServicio);
+                        if (cliente.codigoHTTP == 201)
+                        {
+                            txtID.Text = resultado.id.ToString();
+                            Toast.MakeText(this, "Inserción realizada con éxito", ToastLength.Long).Show();
+                        }
+                        else
+                        {
+                            throw new Exception(cliente.codigoHTTP.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Toast.MakeText(this, "Por favor ingrese el título y el contenido de la entrada", ToastLength.Long).Show();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, "Error: " + ex.Message, ToastLength.Long).Show();
+                }
+            };
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class Entrada
+    {
+        public Entrada()
+        {
+            userId = 1;
+            id = 0;
+            title = "";
+            body = "";
+        }
+
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    public class Cliente
+    {
+        public Cliente()
+        {
+            codigoHTTP = 200;
+        }
+
+        public int codigoHTTP { get; set; }
+
+        //GET
+        public async Task<T> Get<T>(string url)
+        {
+            HttpClient cliente = new HttpClient();
+            var response = await cliente.GetAsync(url);
+            var json = await response.Content.ReadAsStringAsync();
+            codigoHTTP = (int)response.StatusCode;
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        //POST
+        public async Task<T> Post<T>(Entrada item, string url)
+        {
+            HttpClient cliente = new HttpClient();
+            var json = JsonConvert.SerializeObject(item);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+            response = await cliente.PostAsync(url, content);
+            json = await response.Content.ReadAsStringAsync();
+            codigoHTTP = (int)response.StatusCode;
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+
+}
---
+++ 04-ServicioWeb/04-ConsumoServicioWeb/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: c58b987653e36d56dec17e909befb59949b50936 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1147
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300515
+	.include	"typemap.mj.inc"
